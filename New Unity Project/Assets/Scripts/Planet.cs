298adcb0af0272674a264290fad7ca51b07ea81e
pancreasio <julianholisu@gmail.com>
2019-03-31 15:51:58 -0300

    trail and atmospheres added

--- New Unity Project/Assets/Scripts/Planet.cs
+++ New Unity Project/Assets/Scripts/Planet.cs
@@ -6,27 +6,31 @@ namespace gamespace
 {
     public class Planet : MonoBehaviour
     {
-        public Star orbitStar;
+        public GameObject orbitStar;
         public string planetName;
         public float orbitSpeed;
         public float orbitDistance;
         public float initialOrbitAngle = 0;
+        public float rotationSpeed;
+
         private float orbitAngle;
 
         void Start()
         {
-            Debug.Log("planet " + planetName + " initialized");
             orbitAngle = initialOrbitAngle;
         }
 
         // Update is called once per frame
         void Update()
         {
-            Debug.Log(planetName + " is orbiting "+ orbitStar.starName);
+            //orbit star
             transform.position =new Vector3 (orbitStar.transform.position.x + orbitDistance * Mathf.Cos(orbitAngle),
                 orbitStar.transform.position.y,
                 orbitStar.transform.position.z + orbitDistance * Mathf.Sin(orbitAngle));
             orbitAngle += orbitSpeed * Time.deltaTime;
+
+            //planet rotation
+            //transform.rotation = new Vector3(transform.rotation.x + rotationSpeed * Time.deltaTime, transform.rotation.y, transform.rotation.z);
         }
     }
 }
